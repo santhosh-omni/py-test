7cac3f8c34b6a47575c5b0f17ba65a83de4285c6
santhosh-omni <47237191+santhosh-omni@users.noreply.github.com>
2021-08-12 01:48:33 +0530

    Revamp

--- processWebhook.py
+++ processWebhook.py
@@ -27,7 +27,7 @@ def generate_report():
     #Production
     # sqlEngine = create_engine('mysql+pymysql://prod_view:prod_view_22@core-prod.carufofskwa1.ap-southeast-1.rds.amazonaws.com/omnicuris',pool_recycle=36000)
     dbConnection = sqlEngine.connect()
-    queryForCount = 'Select Count(u.id) as count From t_user u '
+    queryForCount = 'Select u.id as count From t_user u Order By u.id DESC LIMIT 1'
     dfCount = pd.read_sql(queryForCount, dbConnection);
     totalCount = dfCount["count"].loc[0]
     print (totalCount)
@@ -43,16 +43,16 @@ def generate_report():
                 'uct.progress, ' \
                 'uct.status ' \
                 'from t_user u ' \
-                'Join t_user_role ur On u.id = ur.user_id ' \
-                'Join t_role r On r.id = ur.role_id ' \
-                'Join t_user_course uc On uc.user_id = u.id ' \
+                'Join t_user_speciality_of_interest soi On soi.user_id = u.id ' \
+                'Join t_course_speciality cs On cs.speciality_id = soi.speciality_id ' \
+                'Join t_course c On c.id = cs.course_id ' \
+                'Join t_user_course uc On uc.user_id = soi.user_id And uc.course_id = c.id  ' \
                 'Join t_user_course_tracker uct On uct.user_course_id = uc.id ' \
-                'Join t_course_speciality cs On cs.course_id = uc.course_id ' \
                 'Join m_speciality s On s.id = cs.speciality_id ' \
                 'Join t_chapter ch On ch.id = uct.widget_id and uct.widget_type = "CHAPTER" ' \
                 'Where uct.is_archived = 0 ' \
                 'And ch.is_webinar = 0 ' \
-                'And ur.role_id Not In (1,4,5,8,10,17,18,23) ' \
+                'And s.id In (Select s.id From m_speciality s) ' \
                 'And u.id Between '
         query += str(startRange)
         query += ' And '
@@ -66,8 +66,9 @@ def generate_report():
         query += str(startRange)
         query += ' And '
         query += str(endRange)
-        query += ' And ur.role_id = 1 Or ur.role_id = 4 Or ur.role_id = 5 Or ur.role_id = 8 Or ur.role_id = 10 Or ur.role_id = 17 Or ur.role_id = 18 Or ur.role_id = 23 Group By u.id)'
-        query += ' Order By u.id asc, ' \
+        query += ' And ur.role_id In (Select id From t_role Where id != 3) Group By u.id) '
+        query += 'Group By soi.speciality_id, soi.user_id ' \
+                 'Order By u.id asc, ' \
                  'uct.id asc'
         print('---------------------------------------')
         print(query)
@@ -78,6 +79,7 @@ def generate_report():
         df = pd.concat([df, dfQuery], ignore_index=True)
     # Calculate progress mean for user with each speciality
     print("****************PROGRESS CALCULATION*********************")
+    df.loc[(df['status'] == "COMPLETED"), ['progress']] = 100
     # Convert the column to int
     df["progress"] = df["progress"].astype(int)
     df['progress_mean'] = df.groupby(['User ID', 'speciality_id'])['progress'].transform('mean')
@@ -86,6 +88,61 @@ def generate_report():
     df = df.drop_duplicates(subset=['User ID', 'speciality_id'], keep='last')
     #Find the mean value for each speciality
     df['mean'] = (df.groupby(['speciality_id'])['progress_mean'].transform('mean'))
+    print("****************Adding Un tracked users*********************")
+    tempDf = None
+    startRange = 0
+    endRange = 10000
+    print('***************Started User Un Tracking For User******************')
+    while startRange<totalCount:
+        query = 'Select u.id as "User ID", ' \
+                's.name as "Speciality Of Interest", ' \
+                's.id as speciality_id, ' \
+                '0 as tracker_id, ' \
+                '0 as progress, ' \
+                '"NOT_STARTED" as status, ' \
+                '0 as progress_mean, ' \
+                '100 as mean ' \
+                'FROM t_user_speciality_of_interest soi ' \
+                'Join t_user u On soi.user_id = u.id ' \
+                'Join m_speciality s On s.id = soi.speciality_id ' \
+                'Where soi.user_id Between '
+        query += str(startRange)
+        query += ' And '
+        query += str(endRange)
+        query += ' And '
+        query += ' soi.speciality_id In (Select s.id From m_speciality s) ' \
+                 'And ' \
+                 'soi.user_id Not In ' \
+                 '(Select u.id From t_user u ' \
+                 'Join t_user_role ur On ur.user_id = u.id ' \
+                 'Where ' \
+                 'u.id Between '
+        query += str(startRange)
+        query += ' And '
+        query += str(endRange)
+        query += ' And ur.role_id In (Select id From t_role Where id != 3) Group By u.id) '
+        query += 'Group By soi.speciality_id, soi.user_id ' \
+                 'Order By u.id asc '
+        print('---------------------------------------')
+        print(query)
+        print('---------------------------------------')
+        startRange = endRange
+        endRange = endRange + 10000
+        dfQuery = pd.read_sql(query, dbConnection)
+        tempDf = pd.concat([tempDf, dfQuery], ignore_index=True)
+    # Concat Records Un tracked records
+    # format
+    tempDf["User ID"] = tempDf["User ID"].astype(int)
+    df["User ID"] = df["User ID"].astype(int)
+    tempDf["speciality_id"] = tempDf["speciality_id"].astype(int)
+    tempDf["User ID"] = tempDf["User ID"].astype(int)
+    df = pd.concat([df, tempDf], ignore_index=True)
+    # Remove duplicates
+    df = df.drop_duplicates(subset=['User ID', 'speciality_id'], keep='first')
+    # sort the records
+    df.sort_values("User ID", axis=0, ascending=True,
+                     inplace=True, na_position='first')
+
     print("****************ENGAGEMENT CALCULATION*********************")
     #Set Engagement level
     df.loc[(df['progress_mean'] > ((df['mean']) + ((df['mean'])*0.1))),['Engagement Level']] = 'VERY_HIGH'
@@ -244,7 +301,7 @@ def generate_report():
         'Activity Level']] = 'LESS'
     print("####################End Activity Level ######################")
 
-    df.to_csv('/home/santhosh-omni/data/data-res-448.csv', index=False)
+    df.to_csv('/home/santhosh-omni/data/data-prod.csv', index=False)
     return "Done"
 
 @app.route('/user-engagement', methods=['POST'])
@@ -257,7 +314,7 @@ def user_engagement():
     print(format(request_data))
     print("**************************************")
     result = None
-    df = pd.read_csv('https://s3.ap-southeast-1.amazonaws.com/omnicuris.assets/marketing/data/final.csv')
+    df = pd.read_csv('https://s3.ap-southeast-1.amazonaws.com/omnicuris.assets/marketing/data/prod/data-prod.csv')
     if 'specialityId' in request_data:
         dfTempSpeciality = None
         if result is None:
@@ -272,7 +329,7 @@ def user_engagement():
         if result is None:
             result = df
         for e in request_data['engagementLevel']:
-            res = result[(result['engagement_level'] == e)]
+            res = result[(result['Engagement Level'] == e)]
             dfTempEngagement = pd.concat([dfTempEngagement, res])
         result = dfTempEngagement
 
@@ -281,7 +338,7 @@ def user_engagement():
         if result is None:
             result = df
         for e in request_data['activityLevel']:
-            res = result[(result['activity_level'] == e)]
+            res = result[(result['Activity Level'] == e)]
             dfTempActivity = pd.concat([dfTempActivity, res])
         result = dfTempActivity
 
@@ -290,7 +347,8 @@ def user_engagement():
     # response.headers.add("Access-Control-Allow-Origin", "*")
     # response = Flask.jsonify({'data': result.to_csv()})
     # response.headers.add("Access-Control-Allow-Origin", "*")
-    result = result.drop(['status', 'speciality_id', 'tracker_id', 'progress_mean', 'progress', 'mean'], axis = 1)
-    # result = result[result['User ID', 'First Name', 'Last Name', 'Email', 'Contact No.', 'Location/Region', 'Speciality Of Interest', 'Registration Date', 'isEmailVerified', 'isDND','engagement_level']]
+    result = result.drop(['status', 'speciality_id', 'tracker_id', 'progress_mean', 'progress', 'mean', 'rep_code', 'Number Of Visits', 'Last Activity Date', 'Activity Level'], axis = 1)
+    result = result[['User ID', 'First Name', 'Last Name', 'Email', 'Contact No.', 'Location/Region', 'Speciality Of Interest', 'Registration Date', 'isEmailVerified', 'isDND','isMCIVerified', 'Engagement Level']]
+    # return result.to_csv(index=False)
     return result.to_csv(index=False)
 
